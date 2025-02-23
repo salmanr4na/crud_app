9e75504368b8521042c3f8feef51110db6cf683a
Salman Rana <raosalmantnb@gmail.com>
2025-02-24 02:32:43 +0530

    second commit

--- db_connection.js
+++ db_connection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose")
 
 mongoose.connect("mongodb://127.0.0.1:27017/mongodb-first")
+// mongoose.connect("mongodb+srv://salmnar4na:Salman12345@cluster0.l5tfz.mongodb.net/")
 
 .then(()=>{
     console.log("database is connected")
     
 }).catch((err)=>{
     console.log(err)
-    console.log("database note connected")
+    console.log("database not connected")
     
 })
